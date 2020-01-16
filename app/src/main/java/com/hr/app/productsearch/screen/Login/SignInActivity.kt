611f4bddf6232d5779eef637bfff3b5a5664475f
igor-lid <onaj.igor@gmail.com>
2020-01-16 18:11:33 +0100

    add signin activity

--- app/src/main/java/com/hr/app/productsearch/screen/Login/SignInActivity.kt
+++ app/src/main/java/com/hr/app/productsearch/screen/Login/SignInActivity.kt
@@ -1,13 +1,14 @@
-package com.hr.app.productsearch
+package com.hr.app.productsearch.screen.Login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hr.app.productsearch.R
 
-class MainActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_signin)
 
         // Hide action bar
         if (supportActionBar != null) supportActionBar?.hide()
