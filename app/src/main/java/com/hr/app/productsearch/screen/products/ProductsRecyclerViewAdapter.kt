80a61e605a3cb72dc85ddbd4bd5f0bf563c34be1
igor-lid <onaj.igor@gmail.com>
2020-01-18 18:39:43 +0100

    add Product rv adapter

---
+++ app/src/main/java/com/hr/app/productsearch/screen/products/ProductsRecyclerViewAdapter.kt
@@ -0,0 +1,56 @@
+package com.hr.app.productsearch.screen.products
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.hr.app.productsearch.R
+import com.hr.app.productsearch.model.Product
+import kotlinx.android.synthetic.main.rv_item_product.view.*
+
+class ProductsRecyclerViewAdapter : RecyclerView.Adapter<ProductsRecyclerViewAdapter.ProductsViewHolder>() {
+    private val onItemClick: ((Product) -> Unit)?
+    private val products: List<Product>
+
+    inner class ProductsViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val productImageView: ImageView = view.findViewById(R.id.productImage)
+        init {
+            view.setOnClickListener {
+                onItemClick?.invoke(products[adapterPosition])
+            }
+        }
+    }
+
+    init {
+        onItemClick = null
+        products = ArrayList()
+    }
+
+    override fun getItemCount(): Int {
+        return products.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductsViewHolder {
+        val view: View = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.rv_item_product, parent, false)
+
+        return ProductsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ProductsViewHolder, position: Int) {
+        val product: Product = products[position]
+
+        Glide.with(holder.itemView.context)
+            .load(product.imageFrontUrl)
+            .placeholder(R.drawable.ic_launcher_background)
+            .into(holder.productImageView)
+
+        holder.itemView.productNameTextView.text = if (product.productName.equals("")) product.productName else "unknown"
+        holder.itemView.productBrandTextView.text = if (product.brands.equals("")) product.brands else "unknown"
+        holder.itemView.productQuantityTextView.text = if (product.quantity.equals("")) product.quantity else "unknown"
+
+    }
+}
