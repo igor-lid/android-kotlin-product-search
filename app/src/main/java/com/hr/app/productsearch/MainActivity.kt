eed42efd5051e34d27a7ce3d3a73c41423f8c942
igor-lid <onaj.igor@gmail.com>
2020-01-19 17:58:42 +0100

    update utils and main activity

--- app/src/main/java/com/hr/app/productsearch/MainActivity.kt
+++ app/src/main/java/com/hr/app/productsearch/MainActivity.kt
@@ -3,7 +3,9 @@ package com.hr.app.productsearch
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hr.app.productsearch.repository.ProductRepository
 import com.hr.app.productsearch.screen.products.ProductsActivity
+import com.hr.app.productsearch.utils.Utils
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,10 +17,30 @@ class MainActivity : AppCompatActivity() {
         // Hide action bar
         if (supportActionBar != null) supportActionBar?.hide()
 
-        // TEST
+        setupOnClickListeners()
+    }
+
+    private fun setupOnClickListeners() {
         search_button.setOnClickListener {
+            val searchValue: String = search_textInput.text.toString()
+
+            // Check if input is empty
+            if (Utils.isTextInputEmpty(searchValue)) {
+                Utils.showSnackbarError(mainLayout, "Please enter text!")
+                return@setOnClickListener
+            }
+            // Set search value in repository
+            ProductRepository.searchValue = searchValue
+
+            // start new activity
             val intent = Intent(it.context, ProductsActivity::class.java)
             it.context.startActivity(intent)
+
+            clearInputs()
         }
     }
+    private fun clearInputs() {
+        search_textInput.text?.clear()
+    }
+
 }
--- app/src/main/java/com/hr/app/productsearch/utils/Utils.kt
+++ app/src/main/java/com/hr/app/productsearch/utils/Utils.kt
@@ -5,18 +5,20 @@ import android.view.View
 import com.google.android.material.snackbar.Snackbar
 
 class Utils {
-    fun showSnackbarLong(layout: View, message: String) {
-        var snackbar = Snackbar.make(layout, message, Snackbar.LENGTH_LONG)
-        snackbar.show()
-    }
+    companion object {
+        fun showSnackbarLong(layout: View, message: String) {
+            var snackbar = Snackbar.make(layout, message, Snackbar.LENGTH_LONG)
+            snackbar.show()
+        }
 
-    fun showSnackbarError(layout: View, message: String) {
-        var snackbar = Snackbar.make(layout, message, Snackbar.LENGTH_LONG)
-        snackbar.setBackgroundTint(Color.RED)
-        snackbar.show()
-    }
+        fun showSnackbarError(layout: View, message: String) {
+            var snackbar = Snackbar.make(layout, message, Snackbar.LENGTH_LONG)
+            snackbar.setBackgroundTint(Color.RED)
+            snackbar.show()
+        }
 
-    fun isTextInputEmpty(text: String): Boolean {
-        return text.isNullOrEmpty() || text.equals("")
+        fun isTextInputEmpty(text: String): Boolean {
+            return text.isNullOrEmpty() || text.equals("")
+        }
     }
 }
